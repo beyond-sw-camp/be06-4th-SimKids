1c51e74d59c00d46f778be14eab43858fc278636
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-08-29 19:17:49 +0900

    [Feat] 새로운 버전

--- backend/src/main/java/org/example/backend/test/TestController.java
+++ backend/src/main/java/org/example/backend/test/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test(){
-        return "기존API";
+        return "새로운API";
     }
 }
